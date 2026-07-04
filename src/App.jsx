8c78c5ee698871182ddfb55f0ada34d90f4b9068
isamary0711 <isarmientosua06@gmail.com>
2026-07-04 00:36:28 -0400

    Corregido nombre de importacion del Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './Landing';
-import login from './login';
+import Login from './Login'; // <--- Cambia 'login' por 'Login' (y asegúrate que el archivo se llame Login.jsx)
 import DashboardInspector from './DashboardInspector';
 import DashboardSupervisor from './DashboardSupervisor';
-import DashboardOperador from './DashboardOperador'; // <-- IMPORTA AQUÍ
+import DashboardOperador from './DashboardOperador';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login />} /> 
         <Route path="/inspector" element={<DashboardInspector />} />
         <Route path="/supervisor" element={<DashboardSupervisor />} />
-        {/* NUEVA RUTA PARA EL OPERADOR */}
         <Route path="/operador" element={<DashboardOperador />} />
       </Routes>
     </BrowserRouter>
